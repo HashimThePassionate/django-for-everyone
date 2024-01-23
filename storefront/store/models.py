78d13b2a104f8949b51eb893baf23c67f6382db4
Muhammad Hashim <hashiimtahir@gmail.com>
2024-01-23 08:37:26 +0500

    Model product and Customer Created

--- storefront/store/models.py
+++ storefront/store/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now=True)
+
+class customer(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=254,unique=True)
+    phone = models.CharField(max_length=255)
+    birth_date = models.DateField(null=True)
+    
