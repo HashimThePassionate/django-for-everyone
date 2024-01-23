3c8cf4b55a215573df0b34577569acdc309ba8bb
Muhammad Hashim <hashiimtahir@gmail.com>
2024-01-23 07:57:43 +0500

    rendering template

--- storefront/storefront/urls.py
+++ storefront/storefront/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from store import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.home ),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
