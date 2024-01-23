87b94acaaef04f4014a9785ac8cbcfd346163501
Muhammad Hashim <hashiimtahir@gmail.com>
2024-01-23 08:52:22 +0500

    Customer Choices and Order Choices created

--- storefront/store/models.py
+++ storefront/store/models.py
@@ -9,9 +9,31 @@ class product(models.Model):
     last_update = models.DateTimeField(auto_now=True)
 
 class customer(models.Model):
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
+    MEMBERSHIP_DIAMOND = 'D' 
+
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_SILVER,'SILVER'),
+        (MEMBERSHIP_GOLD,'GOLD'),
+        (MEMBERSHIP_DIAMOND,'DIAMOND'),
+    ]
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(max_length=254,unique=True)
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
+    membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_SILVER)
+
+class order(models.Model):
+    PAYMENT_PENDING = 'P'
+    PAYMENT_COMPLETE = 'C'
+    PAYMENT_FAILED = 'F'
+    PAYMENT_CHOICES = [
+        (PAYMENT_PENDING,'PENDING'),
+        (PAYMENT_COMPLETE,'COMPLETE'),
+        (PAYMENT_FAILED,'FAILED'),
+    ]
+    place_at = models.DateField(auto_now_add=True)
+    payment_status = models.CharField(max_length=1,choices=PAYMENT_CHOICES,default=PAYMENT_PENDING)
     
