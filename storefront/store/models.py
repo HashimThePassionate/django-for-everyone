ccd735f6fb8bf0b717e3bbd82522af2f4a7c4f26
Muhammad Hashim <hashiimtahir@gmail.com>
2024-01-23 09:21:25 +0500

    Django One to Many  Relationship

--- storefront/store/models.py
+++ storefront/store/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
+    collection = models.ForeignKey(Collection,on_delete=models.PROTECT)
 
 class Customer(models.Model):
     MEMBERSHIP_SILVER = 'S'
@@ -36,9 +41,30 @@ class Order(models.Model):
     ]
     place_at = models.DateField(auto_now_add=True)
     payment_status = models.CharField(max_length=1,choices=PAYMENT_CHOICES,default=PAYMENT_PENDING)
+    customer = models.ForeignKey(Customer,on_delete=models.PROTECT)
 
 class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     customer = models.OneToOneField(Customer,on_delete=models.CASCADE,primary_key = True)
+
+class Address1(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    customer = models.models.ForeignKey(Customer,on_delete=models.CASCADE)
+
+class Orderitem(models.Model):
+    order = models.ForeignKey(Order,on_delete=models.PROTECT)
+    product = models.ForeignKey(Product,on_delete=models.PROTECT)
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class CarItem(models.Model):
+    cart = models.ForeignKey(Cart,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
+
 
