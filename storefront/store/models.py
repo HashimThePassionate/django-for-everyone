74778c1246f4ef42e41485e6cad3c295108fc8d9
Muhammad Hashim <hashiimtahir@gmail.com>
2024-01-23 09:33:38 +0500

    Resolving Circular Relationship

--- storefront/store/models.py
+++ storefront/store/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Promotion(models.Model):
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
+    feature_product = models.ForeignKey('Product',on_delete=models.SET_NULL,null=True,related_name='+')
+
+
 
 class Collection(models.Model):
     title = models.CharField(max_length=255)
@@ -12,6 +18,7 @@ class Product(models.Model):
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection,on_delete=models.PROTECT)
+    promotions = models.ManyToManyField(Promotion)
 
 class Customer(models.Model):
     MEMBERSHIP_SILVER = 'S'
