6cf1e3294258f6fdd9142737a4893a9bc2d822cc
Muhammad Hashim <hashiimtahir@gmail.com>
2024-01-24 07:14:58 +0500

    Django Meta data

--- storefront/store/models.py
+++ storefront/store/models.py
@@ -12,7 +12,7 @@ class Collection(models.Model):
     title = models.CharField(max_length=255)
 
 class Product(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255,verbose_name='Product Tit')
     slug = models.SlugField(default= '-')
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
