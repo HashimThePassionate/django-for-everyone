ae55b8914ad916288d5a7cdd0fef7c115bd9000b
Muhammad Hashim <hashiimtahir@gmail.com>
2024-01-24 06:56:58 +0500

    Add slug field

--- storefront/store/models.py
+++ storefront/store/models.py
@@ -13,6 +13,7 @@ class Collection(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.SlugField(default= '-')
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
