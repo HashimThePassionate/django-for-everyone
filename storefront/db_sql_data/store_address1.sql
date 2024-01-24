aa75e19247f968c2aba0069486b1753311904809
Muhammad Hashim <hashiimtahir@gmail.com>
2024-01-25 04:09:50 +0500

    Dummy data sql

---
+++ storefront/db_sql_data/store_address1.sql
@@ -0,0 +1,9 @@
+insert into store_address1 (id, street, city, customer_id) values (1, 'Bay', 'Kebunkelapa', '6');
+insert into store_address1 (id, street, city, customer_id) values (2, 'Hagan', 'Voorburg', '3');
+insert into store_address1 (id, street, city, customer_id) values (3, 'Westerfield', 'Sampangbitung', '4');
+insert into store_address1 (id, street, city, customer_id) values (4, 'South', 'Xufeng', '3');
+insert into store_address1 (id, street, city, customer_id) values (5, 'Schmedeman', 'Caijiang', '9');
+insert into store_address1 (id, street, city, customer_id) values (6, 'Nevada', 'Trindade', '6');
+insert into store_address1 (id, street, city, customer_id) values (7, 'Green', 'Huzhen', '6');
+insert into store_address1 (id, street, city, customer_id) values (8, 'Oriole', 'Valle de La Pascua', '1');
+insert into store_address1 (id, street, city, customer_id) values (9, 'Fairview', 'Srbica', '6');
