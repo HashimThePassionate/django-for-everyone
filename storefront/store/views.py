6e3daf40f224837dc371f64e37035867a8394522
Muhammad Hashim <hashiimtahir@gmail.com>
2024-01-25 05:19:17 +0500

    filter those product greater than 23

--- storefront/store/views.py
+++ storefront/store/views.py
@@ -6,23 +6,28 @@ from store.models import Product
 
 
 def home(request):
-    query_set = Product.objects.all() 
-    #objects is manager that is a interface of database
+    query_set = Product.objects.all()
+    # objects is manager that is a interface of database
     # for p in query_set:
     #     print(p)
-    # product = Product.objects.get(pk=5) 
+    # product = Product.objects.get(pk=5)
     # print(product)
-    #get only specific record using where clause
+    # get only specific record using where clause
     # try:
     #     product = Product.objects.get(pk=0)
     # except ObjectDoesNotExist:
     #     pass
 
     # product = Product.objects.filter(pk=0).exists()
-    #Filtering Data
-    #find those dollars who have unit_price = 20
-    product = Product.objects.filter(price=20)
-    for p in product:
-            print(product)
+    # Filtering Data
+    # find those dollars who have unit_price = 20
+    # product = Product.objects.filter(price=20)
+    # for p in product:
+    #         print(p)
 
+    # Filter using
+    # keyword argument
+    product = Product.objects.filter(price__gte=23.29)
+    for p in product:
+        print(p)
     return render(request, 'index.html')
