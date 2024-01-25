a915fee9922b3c6b19b81bff38e2f0115ac36d45
Muhammad Hashim <hashiimtahir@gmail.com>
2024-01-25 05:05:22 +0500

    handle objectdoesnotexists error

--- storefront/store/views.py
+++ storefront/store/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product
 
 # Create your views here.
@@ -9,7 +10,12 @@ def home(request):
     #objects is manager that is a interface of database
     # for p in query_set:
     #     print(p)
-    product = Product.objects.get(pk=5) 
-    print(product)
+    # product = Product.objects.get(pk=5) 
+    # print(product)
     #get only specific record using where clause
+    try:
+        product = Product.objects.get(pk=0)
+    except ObjectDoesNotExist:
+        pass
+
     return render(request, 'index.html')
