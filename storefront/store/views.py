f709bb6910ebc8a05468e120fa6e99857bad06a3
Muhammad Hashim <hashiimtahir@gmail.com>
2024-01-25 05:00:20 +0500

    Get only specific record using get method with special argument pk

--- storefront/store/views.py
+++ storefront/store/views.py
@@ -7,6 +7,9 @@ from store.models import Product
 def home(request):
     query_set = Product.objects.all() 
     #objects is manager that is a interface of database
-    for p in query_set:
-        print(p)
+    # for p in query_set:
+    #     print(p)
+    product = Product.objects.get(pk=5) 
+    print(product)
+    #get only specific record using where clause
     return render(request, 'index.html')
