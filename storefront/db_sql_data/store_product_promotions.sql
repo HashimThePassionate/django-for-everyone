9c9e044470cad690ba9657d21fce2028b26d242c
Muhammad Hashim <hashiimtahir@gmail.com>
2024-01-28 02:40:37 +0500

    prefetch_related selection many to many fields

--- storefront/db_sql_data/store_product_promotions.sql
+++ storefront/db_sql_data/store_product_promotions.sql
@@ -1,11 +1,12 @@
-INSERT INTO store_product_promotions()
-VALUES (DEFAULT,1,1),
-(DEFAULT,2,2),
-(DEFAULT,3,5),
-(DEFAULT,6,8),
-(DEFAULT,3,6),
-(DEFAULT,7,9),
-(DEFAULT,7,6),
-(DEFAULT,5,1),
-(DEFAULT,3,4),
-(DEFAULT,8,7);
+INSERT INTO store_product_promotions(id,product_id,promotion_id)
+VALUES 
+(1,1,1),
+(2,2,2),
+(3,3,5),
+(4,6,8),
+(5,3,6),
+(6,7,9),
+(7,7,6),
+(8,5,1),
+(9,3,4),
+(10,8,7);
--- storefront/store/models.py
+++ storefront/store/models.py
@@ -5,7 +5,7 @@ class Promotion(models.Model):
     description = models.CharField(max_length=255)
     discount = models.FloatField()
     featured_product = models.ForeignKey(
-        'Product', on_delete=models.SET_NULL, null=True, related_name='+')
+        'Product', on_delete=models.SET_NULL, null=True, related_name='product_f')
 
 
 class Collection(models.Model):
@@ -13,7 +13,7 @@ class Collection(models.Model):
 
 
 class Product(models.Model):
-    title = models.CharField(max_length=255, verbose_name='Product Tit')
+    title = models.CharField(max_length=255)
     slug = models.SlugField(default='-')
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
