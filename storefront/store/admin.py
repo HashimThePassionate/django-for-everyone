c76d3fd78781e7c9b9dcfa9bdfe1c7735ba25a39
Muhammad Hashim <hashiimtahir@gmail.com>
2024-01-30 08:24:37 +0500

    Adding Computed columns

--- storefront/store/admin.py
+++ storefront/store/admin.py
@@ -3,9 +3,15 @@ from store.models import Collection, Product,Customer
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title','description','price','inventory','collection']
+    list_display = ['title','description','price','inventory','inventory_status','collection']
     list_editable = ['price']
     list_per_page = 10
+    @admin.display(ordering='inventory')
+    def inventory_status(self,Product):
+        if Product.inventory < 3:
+            return 'Low'
+        else:
+            return 'High'
 
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
