03a8927fe84f0f8888859ca65612d9a41e3c9803
Muhammad Hashim <hashiimtahir@gmail.com>
2024-01-30 07:50:53 +0500

    Registering Models on admin site

--- storefront/store/admin.py
+++ storefront/store/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from store.models import Collection, Product
 
-# Register your models here.
+admin.site.register(Collection)
+admin.site.register(Product)
---
+++ storefront/store/models.py
@@ -0,0 +1,102 @@
+from django.db import models
+
+
+class Promotion(models.Model):
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
+    featured_product = models.ForeignKey(
+        'Product', on_delete=models.SET_NULL, null=True, related_name='product_f')
+
+
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.title
+    class Meta:
+        ordering = ['title']
+    
+
+
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(default='-')
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now=True)
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+    promotions = models.ManyToManyField(Promotion)
+
+    def __str__(self):
+        return self.title
+    class Meta:
+        ordering = ['title']
+    
+
+
+class Customer(models.Model):
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
+    MEMBERSHIP_DIAMOND = 'D'
+
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_SILVER, 'SILVER'),
+        (MEMBERSHIP_GOLD, 'GOLD'),
+        (MEMBERSHIP_DIAMOND, 'DIAMOND'),
+    ]
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=254, unique=True)
+    phone = models.CharField(max_length=255)
+    birth_date = models.DateField(null=True)
+    membership = models.CharField(
+        max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_SILVER)
+    points = models.IntegerField(default=50)
+
+
+class Order(models.Model):
+    PAYMENT_PENDING = 'P'
+    PAYMENT_COMPLETE = 'C'
+    PAYMENT_FAILED = 'F'
+    PAYMENT_CHOICES = [
+        (PAYMENT_PENDING, 'PENDING'),
+        (PAYMENT_COMPLETE, 'COMPLETE'),
+        (PAYMENT_FAILED, 'FAILED'),
+    ]
+    place_at = models.DateField(auto_now_add=True)
+    payment_status = models.CharField(
+        max_length=1, choices=PAYMENT_CHOICES, default=PAYMENT_PENDING)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+
+
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    customer = models.OneToOneField(
+        Customer, on_delete=models.CASCADE, primary_key=True)
+
+
+class Address1(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+
+class Orderitem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class CarItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
