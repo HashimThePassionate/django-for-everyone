3978de6861c85ee6efd55eff236712135f4a7ea2
Muhammad Hashim <hashiimtahir@gmail.com>
2024-04-17 06:59:53 +0500

    Extending a user model in authentication

--- storefront/storefront/settings.py
+++ storefront/storefront/settings.py
@@ -37,12 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'django_filters',
     'rest_framework',
     'store',
-    'store_custom',
     'tags',
-    'debug_toolbar',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -146,3 +146,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+
+AUTH_USER_MODEL = 'core.User'
