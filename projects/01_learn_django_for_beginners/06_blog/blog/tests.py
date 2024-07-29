6f59feb4f5c9ede7bebb562208a5efc6abd97e82
HashimThePassionate <hashiimtahir@gmail.com>
2024-07-29 20:22:44 +0500

    All Test Cases Passed

--- projects/01_learn_django_for_beginners/06_blog/blog/tests.py
+++ projects/01_learn_django_for_beginners/06_blog/blog/tests.py
@@ -1,6 +1,7 @@
 # blog/tests.py
 from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse  # new
 from .models import Post
 
 
@@ -22,3 +23,26 @@ class BlogTests(TestCase):
         self.assertEqual(self.post.author.username, "testuser")
         self.assertEqual(str(self.post), "A good title")
         self.assertEqual(self.post.get_absolute_url(), "/post/1/")
+
+    def test_url_exists_at_correct_location_listview(self):  # new
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_exists_at_correct_location_detailview(self):  # new
+        response = self.client.get("/post/1/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_listview(self):  # new
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Nice body content")
+        self.assertTemplateUsed(response, "home.html")
+
+    def test_post_detailview(self):  # new
+        response = self.client.get(reverse("post_detail",
+                                           kwargs={"pk": self.post.pk}))
+        no_response = self.client.get("/post/100000/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(response, "A good title")
+        self.assertTemplateUsed(response, "post_detail.html")
--- projects/01_learn_django_for_beginners/06_blog/blog/views.py
+++ projects/01_learn_django_for_beginners/06_blog/blog/views.py
@@ -9,10 +9,13 @@ def post_list(request):
     return render(request, 'home.html', {'posts': posts})
 
 
-def post_detail(request, pk):
-    try:
-        post = get_object_or_404(Post, pk=pk)
-    except Http404:
-        return render(request, "post_not_found.html", {"message": f"No post available with the primary key {pk}"})
-
+def post_detail(request, pk):  # new
+    post = get_object_or_404(Post, pk=pk)
     return render(request, "post_detail.html", {"post": post})
+# def post_detail(request, pk):
+#     try:
+#         post = get_object_or_404(Post, pk=pk)
+#     except Http404:
+#         return render(request, "post_not_found.html", {"message": f"No post available with the primary key {pk}"})
+
+#     return render(request, "post_detail.html", {"post": post})
