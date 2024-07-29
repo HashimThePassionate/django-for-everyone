b30d503ef6d3841d01b384d35d39308386daf77e
HashimThePassionate <hashiimtahir@gmail.com>
2024-07-29 20:17:16 +0500

    Running Test Cases

--- projects/01_learn_django_for_beginners/06_blog/blog/tests.py
+++ projects/01_learn_django_for_beginners/06_blog/blog/tests.py
@@ -1,3 +1,24 @@
+# blog/tests.py
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from .models import Post
 
-# Create your tests here.
+
+class BlogTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(
+            username="testuser", email="test@email.com", password="secret"
+        )
+        cls.post = Post.objects.create(
+            title="A good title",
+            body="Nice body content",
+            author=cls.user,
+        )
+
+    def test_post_model(self):
+        self.assertEqual(self.post.title, "A good title")
+        self.assertEqual(self.post.body, "Nice body content")
+        self.assertEqual(self.post.author.username, "testuser")
+        self.assertEqual(str(self.post), "A good title")
+        self.assertEqual(self.post.get_absolute_url(), "/post/1/")
--- projects/01_learn_django_for_beginners/06_blog/blog/views.py
+++ projects/01_learn_django_for_beginners/06_blog/blog/views.py
@@ -1,5 +1,6 @@
 # blog/views.py
-from django.shortcuts import render, get_object_or_404 # new
+from django.shortcuts import render, get_object_or_404  # new,
+from django.http import Http404
 from .models import Post
 
 
@@ -7,6 +8,11 @@ def post_list(request):
     posts = Post.objects.all()
     return render(request, 'home.html', {'posts': posts})
 
-def post_detail(request, pk): # new
-    post = get_object_or_404(Post, pk=pk)
+
+def post_detail(request, pk):
+    try:
+        post = get_object_or_404(Post, pk=pk)
+    except Http404:
+        return render(request, "post_not_found.html", {"message": f"No post available with the primary key {pk}"})
+
     return render(request, "post_detail.html", {"post": post})
