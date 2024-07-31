f171acab6833acc9f8bf95d2ba62ed9109762811
HashimThePassionate <hashiimtahir@gmail.com>
2024-07-31 22:06:45 +0500

    Custom User Model

---
+++ projects/01_learn_django_for_beginners/09_newspaper_project/accounts/forms.py
@@ -0,0 +1,15 @@
+# accounts/forms.py
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import CustomUser
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = UserCreationForm.Meta.fields + ("age",)
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = CustomUser
+        fields = UserChangeForm.Meta.fields
--- projects/01_learn_django_for_beginners/09_newspaper_project/accounts/models.py
+++ projects/01_learn_django_for_beginners/09_newspaper_project/accounts/models.py
@@ -1,3 +1,7 @@
+# accounts/models.py
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    age = models.PositiveIntegerField(null=True, blank=True)
