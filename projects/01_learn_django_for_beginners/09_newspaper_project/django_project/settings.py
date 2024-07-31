05b110586b4e91c44bc4460b21d5be0a4333db1f
HashimThePassionate <hashiimtahir@gmail.com>
2024-07-31 20:39:56 +0500

    Custom User Model

--- projects/01_learn_django_for_beginners/09_newspaper_project/django_project/settings.py
+++ projects/01_learn_django_for_beginners/09_newspaper_project/django_project/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts',  # new
 ]
+...
+AUTH_USER_MODEL = "accounts.CustomUser"  # new
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
