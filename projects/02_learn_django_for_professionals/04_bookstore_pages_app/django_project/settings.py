5308d15180d3fac94cd0e77bfb17492e7b32016c
Muhammad Hashim <hashiimtahir@gmail.com>
2024-08-15 23:57:02 +0500

    Setting Up Templates

--- projects/02_learn_django_for_professionals/04_bookstore_pages_app/django_project/settings.py
+++ projects/02_learn_django_for_professionals/04_bookstore_pages_app/django_project/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
