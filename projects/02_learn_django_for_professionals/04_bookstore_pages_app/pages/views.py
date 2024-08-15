1537ed06b6c85434ca5db42da7e733cecc70647f
Muhammad Hashim <hashiimtahir@gmail.com>
2024-08-16 00:08:58 +0500

    Urls and Views

--- projects/02_learn_django_for_professionals/04_bookstore_pages_app/pages/views.py
+++ projects/02_learn_django_for_professionals/04_bookstore_pages_app/pages/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+# pages/views.py
 
-# Create your views here.
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+    template_name = "home.html"
