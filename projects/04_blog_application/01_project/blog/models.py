4f18bde0e396be7212e9c0ffd7898fe57348471e
HashimThePassionate <hashiimtahir@gmail.com>
2024-11-26 07:32:36 +0500

    Creating the Post Model �

---
+++ projects/04_blog_application/01_project/blog/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
