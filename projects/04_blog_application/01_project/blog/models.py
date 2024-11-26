7c720e1d06d50ce6ecfa2fa064bedbdfdcbca7bf
HashimThePassionate <hashiimtahir@gmail.com>
2024-11-26 08:21:58 +0500

    Adding a Status Field with Enumeration Types in Django

--- projects/04_blog_application/01_project/blog/models.py
+++ projects/04_blog_application/01_project/blog/models.py
@@ -1,13 +1,24 @@
 from django.db import models
 from django.utils import timezone
 
+
 class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
-    publish = models.DateTimeField(default=timezone.now)      
-    created = models.DateTimeField(auto_now_add=True)     
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=2,
+        choices=Status.choices,
+        default=Status.DRAFT                         
+    )
+
     class Meta:
         ordering = ['-publish']
         indexes = [models.Index(fields=['-publish']),]
