515fdfd844370dc3a3a55bd633cdaa26eb8fe5b8
HashimThePassionate <hashiimtahir@gmail.com>
2024-11-27 07:13:05 +0500

    Understanding Many-to-One Relationships in Django

--- projects/04_blog_application/01_project/blog/models.py
+++ projects/04_blog_application/01_project/blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils import timezone
-
+from django.conf import settings
 
 class Post(models.Model):
     class Status(models.TextChoices):
@@ -9,6 +9,11 @@ class Post(models.Model):
 
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(
+    settings.AUTH_USER_MODEL,
+    on_delete=models.CASCADE,
+    related_name='blog_posts'
+    )
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
