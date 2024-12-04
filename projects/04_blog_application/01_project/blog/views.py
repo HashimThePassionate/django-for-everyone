373c403b527fb8817a15fd15af74c8dfdb237e8f
HashimThePassionate <hashiimtahir@gmail.com>
2024-12-04 07:44:34 +0500

    Class-Based Views in Django

--- projects/04_blog_application/01_project/blog/views.py
+++ projects/04_blog_application/01_project/blog/views.py
@@ -1,25 +1,38 @@
 from django.core.paginator import EmptyPage, PageNotAnInteger,  Paginator
 from django.shortcuts import render,  get_object_or_404
 from .models import Post
+from django.views.generic import ListView
 
-def post_list(request):
-    post_list = Post.published.all()  # Retrieve all posts with status=PUBLISHED
-    # Pagination with 3 posts per page
-    paginator = Paginator(post_list, 3)
-    page_number = request.GET.get('page', 1)
-    try:
-        posts = paginator.page(page_number)
-    except PageNotAnInteger:
-        # If page_number is not an integer get the first page
-        posts = paginator.page(1)
-    except EmptyPage:
-        # If page_number is out of range get last page of results
-        posts = paginator.page(paginator.num_pages)        
-    return render(
-        request, 
-        'blog/post/list.html',  # Specify the template
-        {'posts': posts}  # Pass the posts as context
-    )
+
+class PostListView(ListView):
+    """
+    Alternative post list view
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+
+# def post_list(request):
+#     post_list = Post.published.all()  # Retrieve all posts with status=PUBLISHED
+#     # Pagination with 3 posts per page
+#     paginator = Paginator(post_list, 3)
+#     page_number = request.GET.get('page', 1)
+#     try:
+#         posts = paginator.page(page_number)
+#     except PageNotAnInteger:
+#         # If page_number is not an integer get the first page
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         # If page_number is out of range get last page of results
+#         posts = paginator.page(paginator.num_pages)
+#     return render(
+#         request,
+#         'blog/post/list.html',  # Specify the template
+#         {'posts': posts}  # Pass the posts as context
+#     )
+
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(
